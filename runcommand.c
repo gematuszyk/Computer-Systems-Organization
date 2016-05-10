a7a9f737ad6590a7e913c9b04b93344dbab15aeb
gracematuszyk <gem306@nyu.edu>
2016-05-10 11:21:02 -0400

    Submitting Lab 4

--- runcommand.c
+++ runcommand.c
@@ -1,20 +1,5 @@
 #include "lab4.h"
 
-void childhandler() {
-        
-        pid_t pid1;
-        int status1;
-    
-        while(1) {
-            pid1 = waitpid(pid1,&status1,WNOHONG);
-            if(pid1 == 0) {
-                return;
-            }else if(pid == -1) {
-                return;
-            }
-        }
-}
-
 // Execute a command with optional wait
 int runcommand(char** cline, int where)
 {
@@ -22,8 +7,38 @@ int runcommand(char** cline, int where)
     int status;
     char* path;
 `
-    signal(SIGCHLD, childhandler);
+    switch(pid = fork())
+    {
+        case -1:
+                perror(SHELL_NAME);
+                return (-1);
+        case 0:
+                if(where == BACKGROUND) {
+                        signal(SIGINT, handler);
+                }
+                if(strcmp(cline[0], "cd")==0) {
+                        exit(0);
+                }
+                else if(strcmp(cline[0], "exit")==0) {
+                        exit(0);
+                }
+                else {
+                        execvp(*cline,cline);
+                        //we should never get to this code, sice execve does not return
+                        perror(*cline);
+                        exit(1);
+                }break;
+        default:
+                break;
+    }
 
+    // parent
+    // If background process print pid and exit program is running in the background
+    if(where == BACKGROUND)
+    {
+        printf("[Process id %d]\n",pid);
+        return 0;
+    }
     if(strcmp(cline[0], "exit")==0) {
         exit(0);
     }
@@ -36,34 +51,17 @@ int runcommand(char** cline, int where)
             chdir(path);
         }
     }
-    else {
-        switch(pid = fork())
-        {
-            case -1:
-                perror(SHELL_NAME);
-                return (-1);
-            case 0:
-                execvp(*cline,cline);
-                //we should never get to this code, sice execve does not return
-                perror(*cline);
-                exit(1);
-        }
-    }
-    // Code for parent
-    // If background process print pid and exit program is running in the background
-    if(where == BACKGROUND)
-    {
-        printf("[Process id %d]\n",pid);
-        return 0;
-    }
 
+    //child
     // Wait until process pid exits
-    if (waitpid(pid,&status,0) == -1) 
-    {
-        return -1;
-    } 
-    else 
-    {
-        return status;
+    if(where == FOREGROUND) {
+            if (waitpid(pid,&status,0) == -1) 
+            {
+                return -1;
+            } 
+            else 
+            {
+                return status;
+            }
     }
 }
