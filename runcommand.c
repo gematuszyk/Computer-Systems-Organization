82fa28208c4aa29adbfe5ec1f2c5c3e9cc888943
gracematuszyk <gem306@nyu.edu>
2016-05-09 21:55:14 -0400

    Submitting Lab 4

--- runcommand.c
+++ runcommand.c
@@ -5,19 +5,33 @@ int runcommand(char** cline, int where)
 {
     pid_t pid;
     int status;
+    char* path;
 
-    switch(pid = fork())
-    {
-        case -1:
-            perror(SHELL_NAME);
-            return (-1);
-        case 0:
-            execvp(*cline,cline);
-            //we should never get to this code, sice execve does not return
-            perror(*cline);
-            exit(1);
+    if(strcmp(cline[0], "exit")==0) {
+        exit(0);
     }
-
+    else if(strcmp(cline[0], "cd")==0) {
+        if(cline[1]!=NULL) {
+            chdir(cline[1]);
+        }
+        else {
+            path = getenv("HOME");
+            chdir(path);
+        }
+    }
+    else {
+        switch(pid = fork())
+        {
+            case -1:
+                perror(SHELL_NAME);
+                return (-1);
+            case 0:
+                execvp(*cline,cline);
+                //we should never get to this code, sice execve does not return
+                perror(*cline);
+                exit(1);
+        }
+    }
     // Code for parent
     // If background process print pid and exit program is running in the background
     if(where == BACKGROUND)
@@ -29,6 +43,7 @@ int runcommand(char** cline, int where)
     // Wait until process pid exits
     if (waitpid(pid,&status,0) == -1) 
     {
+        waitpid(pid,&status,WNOHONG);
         return -1;
     } 
     else 
